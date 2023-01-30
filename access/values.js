c29704bb235b7dee6d8b238d26dae7c368c76af7
adesar12345 <102373741+adesar12345@users.noreply.github.com>
2023-01-31 01:42:16 +0200

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "14.09.2004"; // Дата рождения
+var fullname = "Мала Вікторія Борисівна" // ФИО
+var name = "Вікторія" // Имя
+
+// Паспорт
+var passport_id = "004185972" // Номер паспорта
+
+// КПП
+var kpp_id = "3862001125" // ИНН
+
+// Права
+var rights_valid_until = "30.7.2025" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "27.12.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
